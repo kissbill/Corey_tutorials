b66c2b50c914d592c2e20f42a85159acaeae5606
kissbill <milerik@hotmail.com>
2020-05-07 19:18:41 +0200

    add Generator tutorial

---
+++ Python_tutorial/00_Generators_square_nums.py
@@ -0,0 +1,43 @@
+# 1. pelda kod ########################################################
+# def square_numbers(nums):
+# 	result = []
+# 	for i in nums:
+# 		result.append(i * i)
+# 	return result
+
+# my_nums = square_numbers([1,2,3,4,5])
+
+
+# print(my_nums)
+
+
+# 2. pelda kod ########################################################
+# def square_numbers(nums):
+# 	for i in nums:
+# 		yield (i * i) 
+# 		# 1 result at a time
+
+# my_nums = square_numbers([1,2,3,4,5])
+# print(next(my_nums))
+# print(next(my_nums))
+# print(next(my_nums))
+# print(next(my_nums))
+# print(next(my_nums))
+# print(my_nums)
+
+# for num in my_nums:
+# 	print(num)
+
+# 3. pelda kod ########################################################
+# memoriaba kerul
+# my_nums = [x*x for x in [1,2,3,4,5]]
+
+# print(my_nums)
+
+# 4. pelda kod ########################################################
+# GENERATOR - nincs memoriaban
+my_nums = (x*x for x in [1,2,3,4,5])
+
+# listava konvertalom
+# viszont elvesztem az perfomance elonyt urja memoriat olvass
+print(list(my_nums))
---
+++ Python_tutorial/01_Generators_people_exmpl.py
@@ -0,0 +1,42 @@
+import memory_profiler
+import random
+import time
+
+names = ['John', 'Corey', 'Adam', 'Steve', 'Rick', 'Thomas']
+majors = ['Math', 'Engineering', 'CompSci', 'Arts', 'Business']
+
+print ('Memory (Before): {}Mb'.format(memory_profiler.memory_usage()))
+
+def people_list(num_people):
+    result = []
+    for i in range(num_people):
+        person = {
+                    'id': i,
+                    'name': random.choice(names),
+                    'major': random.choice(majors)
+                }
+        result.append(person)
+    return result
+
+def people_generator(num_people):
+    for i in range(num_people):
+        person = {
+                    'id': i,
+                    'name': random.choice(names),
+                    'major': random.choice(majors)
+                }
+        yield person
+
+# t1 = time.clock()
+# people = people_list(1000000)
+# t2 = time.clock()
+
+t1 = time.clock()
+people = people_generator(1000000)
+t2 = time.clock()
+
+print('Memory (After) : {} Mb'.format(memory_profiler.memory_usage()))
+print('Took {} Seconds'.format(t2-t1))
+
+# memory_profiler issue
+# https://stackoverflow.com/questions/41191412/no-module-named-mem-profile
